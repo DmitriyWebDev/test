c00c52a933b7926707f1de44618f20903cb0c8e1
Dmitriy Gavrilov <dmitriy.gavrilov@cimpleo.com>
2017-08-14 00:54:28 +0700

    add NODE_ENV

--- webpack.config.js
+++ webpack.config.js
@@ -5,98 +5,113 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 const webpack = require("webpack");
 
-module.exports = [
-  // JavaScript
-  {
-    context: path.join(__dirname, "src", 'js'),
-    entry: {
-      main: './main.js'
-    },
-    output: {
-      filename: './js/[name].bundle.js',
-      path: path.resolve(__dirname, 'assets')
-    },
-    watch   : true,
-    devtool: "source-map",
-    plugins: [
-      new webpack.ProvidePlugin( {
-        $                : "jquery",
-        jQuery           : "jquery",
-        "windows.jQuery" : "jquery"
-      } ),
-      new UglifyJSPlugin({
-        sourceMap: true,
-        exclude : /node_modules/
-      })
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+module.exports = [];
+
+// JavaScript
+let js_config = {
+  context: path.join(__dirname, "src", 'js'),
+  entry: {
+    main: './main.js'
+  },
+  output: {
+    filename: './js/[name].bundle.js',
+    path: path.resolve(__dirname, 'assets')
+  },
+  watch: true,
+  devtool: "source-map",
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      "windows.jQuery": "jquery"
+    })
+  ]
+};
+
+if (NODE_ENV == 'production') {
+  js_config.plugins.push(
+    new UglifyJSPlugin({
+      sourceMap: true,
+      exclude: /node_modules/
+    })
+  );
+}
+
+module.exports.push(js_config);
+
+// Css ( included Fonts, Background - images for css - styles )
+
+let minimize_css = false;
+
+if (NODE_ENV == 'production') {
+  minimize_css = true;
+}
+
+let css_config = {
+  context: path.join(__dirname, "src", 'sass'),
+  entry: {
+    main: './main.scss',
+    // 'common' : './common.scss' // for creating another css - files
+  },
+  output: {
+    filename: './css/[name].bundle.css',
+    path: path.resolve(__dirname, 'assets')
+  },
+  watch: true,
+  devtool: "source-map",
+  module: {
+    rules: [
+      // Sass
+      {
+        test: /\.scss$/,
+        use: ExtractTextPlugin.extract({
+          use: [
+            {loader: 'css-loader', options: {sourceMap: true, minimize: minimize_css}},
+            {loader: 'sass-loader', options: {sourceMap: true}},
+            // {loader: 'source-map-loader', options: {sourceMap: true}} // test for another source-maps
+          ]
+        })
+      },
+      // Fonts
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '../',
+              outputPath: 'fonts/',
+              name: '[name]/[name].[ext]'
+            }
+          }
+        ]
+      },
+      // Images for css property - background
+      {
+        test: /\.(jpe?g|png|gif|svg)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              publicPath: '../',
+              outputPath: 'img/',
+              name: '[name].[ext]'
+            }
+          }
+        ]
+      }
     ]
   },
-  // Css ( included Fonts, Background - images for css - styles )
-  {
-    context: path.join(__dirname, "src", 'sass'),
-    entry: {
-      main: './main.scss',
-      // 'common' : './common.scss' // for creating another css - files
-    },
-    output: {
-      filename: './css/[name].bundle.css',
-      path: path.resolve(__dirname, 'assets')
-    },
-    watch   : true,
-    devtool: "source-map",
-    module: {
-      rules: [
-        // Sass
-        {
-          test: /\.scss$/,
-          use: ExtractTextPlugin.extract({
-            use: [
-              {loader: 'css-loader', options: {sourceMap: true, minimize: false}},
-              {loader: 'sass-loader', options: {sourceMap: true}},
-              // {loader: 'source-map-loader', options: {sourceMap: true}} // test for another source-maps
-            ]
-          })
-        },
-        // Fonts
-        {
-          test: /\.(eot|ttf|woff|woff2)$/,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                publicPath: '../',
-                outputPath: 'fonts/',
-                name: '[name]/[name].[ext]'
-              }
-            }
-          ]
-        },
-        // Images for css property - background
-        {
-          test    : /\.(jpe?g|png|gif|svg)$/i,
-          use: [
-            {
-              loader: 'file-loader',
-              options: {
-                publicPath: '../',
-                outputPath: 'img/',
-                name: '[name].[ext]'
-              }
-            }
-          ]
-        }
-      ]
-    },
-    plugins: [
-      new ExtractTextPlugin({
-        filename: './css/[name].bundle.css'
-      })
-    ]
-  }
-];
+  plugins: [
+    new ExtractTextPlugin({
+      filename: './css/[name].bundle.css'
+    })
+  ]
+};
 
-
-
-
+module.exports.push(css_config);
 
 
 
